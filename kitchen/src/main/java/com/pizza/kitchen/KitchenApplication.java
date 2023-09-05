e31647cb18d827ea2fbff8944fba45541c405cd8
Denis Magda <dmagda@gridgain.com>
2023-09-05 10:45:08 -0400

    removed spring cloud dependencies

--- kitchen/src/main/java/com/pizza/kitchen/KitchenApplication.java
+++ kitchen/src/main/java/com/pizza/kitchen/KitchenApplication.java
@@ -2,10 +2,8 @@ package com.pizza.kitchen;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class KitchenApplication {
 
     public static void main(String[] args) {
--- tracker/src/main/java/com/pizza/tracker/TrackerApplication.java
+++ tracker/src/main/java/com/pizza/tracker/TrackerApplication.java
@@ -2,10 +2,8 @@ package com.pizza.tracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class TrackerApplication {
 
     public static void main(String[] args) {
